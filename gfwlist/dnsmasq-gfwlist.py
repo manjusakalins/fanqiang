7f95931362e977bbb944a8d9558c4225934eeab1
linhuasheng <linhuasheng@jlinksz.com>
2016-07-08 17:58:48 +0800

    update gfwlist renew when ss start try fix size not enough

--- gfwlist/dnsmasq-gfwlist.py
+++ gfwlist/dnsmasq-gfwlist.py
@@ -1,36 +1,37 @@
-#!/usr/bin/env python  
+#!/usr/bin/env python
 #coding=utf-8
-#  
+#
 # Generate a list of dnsmasq rules with ipset for gfwlist
-#  
-# Copyright (C) 2014 http://www.shuyz.com   
-# Ref https://code.google.com/p/autoproxy-gfwlist/wiki/Rules    
- 
-import urllib2 
+#
+# Copyright (C) 2014 http://www.shuyz.com
+# Ref https://code.google.com/p/autoproxy-gfwlist/wiki/Rules
+
+import urllib2
 import re
 import os
 import datetime
 import base64
 import shutil
- 
-mydnsip = '127.0.0.1'
-mydnsport = '1053'
+
+mydnsip = '208.67.222.222'
+mydnsport = '443'
 
 # the url of gfwlist
 baseurl = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
 # match comments/title/whitelist/ip address
 comment_pattern = '^\!|\[|^@@|^\d+\.\d+\.\d+\.\d+'
-domain_pattern = '([\w\-\_]+\.[\w\.\-\_]+)[\/\*]*' 
+domain_pattern = '([\w\-\_]+\.[\w\.\-\_]+)[\/\*]*'
 tmpfile = '/tmp/gfwlisttmp'
 # do not write to router internal flash directly
 outfile = '/tmp/gfwlist.conf'
+filename = 'lhs_gfwlist.conf'
 rulesfile = '/etc/dnsmasq.d/gfwlist.conf'
- 
+
 fs =  file(outfile, 'w')
 fs.write('# gfw list ipset rules for dnsmasq\n')
 fs.write('# updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
 fs.write('#\n')
- 
+
 print 'fetching list...'
 content = urllib2.urlopen(baseurl, timeout=15).read().decode('base64')
 
@@ -45,7 +46,7 @@ print 'page content fetched, analysis...'
 # remember all blocked domains, in case of duplicate records
 domainlist = []
 
-for line in tfs.readlines():	
+for line in tfs.readlines():
 	if re.findall(comment_pattern, line):
 		print 'this is a comment line: ' + line
 		#fs.write('#' + line)
@@ -62,14 +63,17 @@ for line in tfs.readlines():
 				fs.write('ipset=/.%s/gfwlist\n'%domain[0])
 		else:
 			print 'no valid domain in this line: ' + line
-					
-tfs.close()	
+
+tfs.close()
 fs.close();
+
+os.popen("cp "+ outfile + " ./"+filename)
+os.popen("tar zcvf "+ filename + ".tar " + filename);
 
 print 'moving generated file to dnsmasg directory'
 shutil.move(outfile, rulesfile)
 
 print 'restart dnsmasq...'
 print os.popen('/etc/init.d/dnsmasq restart').read()
- 
+
 print 'done!'
